9f0a131f25a39ac15498b2dfeab3d2a60fccb902
Paul Thomas <paulthomas2107@gmail.com>
2023-05-26 13:06:59 +0100

    More

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,30 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { localStorageStore } from '@skeletonlabs/skeleton'
+	import type { Writable } from 'svelte/store';
+
+	let inputName = ""
+	let inputPhoneNumber = ""
+
+	interface Contact {
+		name: string
+		phoneNumber: string
+	}
+
+	const contactStore: Writable<Contact[]> = localStorageStore("contactStore", [])
+
+	function addContact() {
+
+		$contactStore = [
+			{
+				name: "",
+				phoneNumber: ""
+			},
+			...$contactStore
+		]
+	}
+
+
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
@@ -6,12 +32,14 @@
 		<p>All your contacts in one persistent state!</p>
 		<label class="label">
 			<span>Name</span>
-			<input class="input" type="text" placeholder="Name">
+			<input class="input" type="text" placeholder="Name" bind:value={inputName}>
 		</label>
 		<label class="label">
 			<span>Phone Number</span>
-			<input class="input" type="text" placeholder="Phone Number">
+			<input class="input" type="text" placeholder="Phone Number" bind:value={inputPhoneNumber}>
 		</label>
 		<button type="button" class="btn variant-filled">Add Contact</button>
+		<hr>
+		<h2 class="h2">Your Contacts</h2>
 	</div>
 </div>
