3227a6597dfd3e01536fcd803e05c45ddbbd7dee
Paul Thomas <paulthomas2107@gmail.com>
2023-05-26 12:50:22 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Contact List</h1>
+		<p>All your contacts in one persistent state!</p>
+		<label class="label">
+			<span>Name</span>
+			<input class="input" type="text" placeholder="Name">
+		</label>
+		<label class="label">
+			<span>Phone Number</span>
+			<input class="input" type="text" placeholder="Phone Number">
+		</label>
+		<button type="button" class="btn variant-filled">Add Contact</button>
+	</div>
+</div>
